a3a1ef6fc554ad959a6963cd41226fb1c5bb489d
appledav-id <appledav000@gmail.com>
2021-11-02 12:12:42 -0500

    Small modifications to app, getting familiar with button clicks and whatnot

--- app/src/main/java/com/example/sdcircuitsolver/MainActivity.java
+++ app/src/main/java/com/example/sdcircuitsolver/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sdcircuitsolver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onButtonClick(View view)
+    {
+        TextView txtWelcomeMsg = findViewById(R.id.welcomeMsg);
+        txtWelcomeMsg.setText("Will setup TCP connection and send message");
     }
 }
